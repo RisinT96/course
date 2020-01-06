7a103a4d0217b09cdb099b344a0b8b258ac16b46
Tal Risin <risint96@gmail.com>
2020-01-06 16:46:30 +0200

    crap

--- sol/context/ex_2.py
+++ sol/context/ex_2.py
@@ -1,3 +1,5 @@
+import orjson
+import json
 from contextlib import contextmanager
 
 
@@ -10,3 +12,34 @@ def Timed(str):
     stop = time.time()
 
     print(f'{str} took {stop - start}s')
+
+
+with Timed('json loads'):
+    for i in range(1000):
+        with open("github.json", 'r') as f:
+            loaded_json = json.loads(f.read())
+
+with Timed('json load'):
+    for i in range(1000):
+        with open("github.json", 'r') as f:
+            loaded_json = json.load(f)
+
+with Timed('orjson loads'):
+    for i in range(1000):
+        with open("github.json", 'r') as f:
+            loaded_orjson = orjson.loads(f.read())
+
+with Timed('json dumps'):
+    for i in range(1000):
+        with open("github.json,out", 'w') as f:
+            f.write(json.dumps(loaded_json))
+
+with Timed('json dump'):
+    for i in range(1000):
+        with open("github.json.out", 'w') as f:
+            json.dump(loaded_json, f)
+
+with Timed('orjson dumps'):
+    for i in range(1000):
+        with open("github.json.out", 'wb') as f:
+            f.write(orjson.dumps(loaded_json))
