c4f388353c58fe6a0f3a4569748c80fa7ba8d6f0
Tal Risin <risint96@gmail.com>
2020-01-06 10:37:54 +0200

    Fixed keydefaultdict

--- sol/stl/keydefaultdict.py
+++ sol/stl/keydefaultdict.py
@@ -3,14 +3,7 @@ from collections import defaultdict
 
 class keydefaultdict(defaultdict):
     def __missing__(self, key):
-        return super().default_factory(key)
-
-a = keydefaultdict(int)
-
-a[50] += 1
-a[0x50] += 0x5
-
-print(a['0']*10)
-print(a['2']*10)
-print(a['3']*10)
-print(a[0x50])
+        if self.default_factory is None:
+            raise KeyError("No default factory provided!")
+        self[key] = self.default_factory(key)
+        return self[key]
